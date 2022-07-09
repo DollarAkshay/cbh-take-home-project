e3144f4d42c97e48073d5361570eb43e9c89b420
Akshay.L.Aradhya <akshay95aradhya@gmail.com>
2022-07-10 02:53:03 +0530

    Adding jest tests

--- dpk.js
+++ dpk.js
@@ -1,28 +1,55 @@
-const crypto = require("crypto");
+import crypto from 'crypto'
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = '0'
+const MAX_PARTITION_KEY_LENGTH = 256
+
+export const _deterministicPartitionKey = (event) => {
+  const TRIVIAL_PARTITION_KEY = '0'
+  const MAX_PARTITION_KEY_LENGTH = 256
+  let candidate
 
   if (event) {
     if (event.partitionKey) {
-      candidate = event.partitionKey;
+      candidate = event.partitionKey
     } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+      const data = JSON.stringify(event)
+      candidate = crypto.createHash('sha3-512').update(data).digest('hex')
     }
   }
 
   if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
+    if (typeof candidate !== 'string') {
+      candidate = JSON.stringify(candidate)
     }
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    candidate = TRIVIAL_PARTITION_KEY
   }
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    candidate = crypto.createHash('sha3-512').update(candidate).digest('hex')
   }
-  return candidate;
-};
+  return candidate
+}
+
+export const deterministicPartitionKey = (event) => {
+  // If the event object is null or undefined, return the trivial key.
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY
+  }
+
+  // If the event object has a partition key, and it is not a string, stringify it using JSON.stringify.
+  if (event.partitionKey && typeof event.partitionKey !== 'string') {
+    event.partitionKey = JSON.stringify(event.partitionKey)
+  }
+
+  // If the event object has a partition key that is the candidate,
+  // else JSON.stringify the event object and make it the candidate.
+  let candidate = event?.partitionKey || JSON.stringify(event)
+
+  // If the candidate is longer than the maximum partition key length,
+  // hash it and make it the candidate.
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = crypto.createHash('sha3-512').update(candidate).digest('hex')
+  }
+
+  return candidate
+}
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,52 @@
-const { deterministicPartitionKey } = require("./dpk");
+const {
+  _deterministicPartitionKey,
+  deterministicPartitionKey
+} = require('./dpk')
 
-describe("deterministicPartitionKey", () => {
+describe('deterministicPartitionKey', () => {
   it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
-    expect(trivialKey).toBe("0");
-  });
-});
+    const oldKey = _deterministicPartitionKey()
+    const newKey = deterministicPartitionKey()
+
+    expect(oldKey).toEqual(newKey)
+    expect(newKey).toBe('0')
+  })
+
+  it('Returns the sha3-512 hash when given table data without partitionKey', () => {
+    const oldKey = _deterministicPartitionKey([1, 2, 3, {}])
+    const newKey = extractPartitionKey([1, 2, 3, {}])
+
+    expect(oldKey).toEqual(newKey)
+    expect(newKey).toMatch(/^[0-9a-f]{128}$/)
+  })
+
+  it('Returns the partition key if it is less than or equal to 256 characters', () => {
+    const len256String = new Array(257).join('1')
+
+    const oldKey = _deterministicPartitionKey({ partitionKey: len256String })
+    const newKey = deterministicPartitionKey({ partitionKey: len256String })
+
+    expect(oldKey).toEqual(newKey)
+    expect(newKey).toEqual(len256String)
+  })
+
+  it('Returns the sha3-512 hash of the partition key if it is greater than 256 characters', () => {
+    const len257String = new Array(258).join('1')
+
+    const oldKey = _deterministicPartitionKey({ partitionKey: len257String })
+    const newKey = deterministicPartitionKey({ partitionKey: len257String })
+
+    expect(oldKey).toEqual(newKey)
+    expect(trivialKey).toMatch(/^[0-9a-f]{128}$/)
+  })
+
+  it('Returns the JSON.stringify if the event.partitionKey is not a string and the JSON string is less than or equal to 256', () => {
+    const arr = [1, 2, 3]
+
+    const oldKey = _deterministicPartitionKey({ partitionKey: arr })
+    const newKey = deterministicPartitionKey({ partitionKey: arr })
+
+    expect(oldKey).toEqual(newKey)
+    expect(newKey).toEqual(JSON.stringify(arr))
+  })
+})
